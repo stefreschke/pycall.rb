748dd1e8f2e71187a4e02ba3f50305749f7fb1db
Stefan Reschke <stefan.reschke@outlook.de>
2020-08-20 13:14:53 +0200

    Nicify benchmark

--- benchall.py
+++ benchall.py
@@ -6,26 +6,30 @@ import os
 # export PYTHONPATH=$(pwd)
 #
 #first
-truffleruby = "/home/path/graalvm-ce-java11-20.1.0/bin/ruby"
+truffleruby = "~/Runtimes/graalvm-ce-java11-20.1.0/bin/ruby"
 
-tests = [10000, 10, 100, 500, 1000, 5000, 25000, 50000]
+tests = [i for i in range(1, 101, 1)]
+PYCALL_RB, TRUFFLERUBY_PYCALL, TRUFFLERUBY_POLYGLOT = range(3)
+modes = [PYCALL_RB, TRUFFLERUBY_PYCALL, TRUFFLERUBY_POLYGLOT]
 
+print("doing ", tests)
 print("iterations,ruby,truffleruby,trufflerubypolyglot")
+
 for k in tests:
-
     print(str(k)+",", end='', flush=True)
-    for mode in range(1, 4):
-        if os.path.exists("bench.txt"): os.remove("bench.txt")
-        if mode == 1: os.system("ruby benchmark.rb " + str(k) + " > /dev/null")
-        if mode == 2: os.system(truffleruby + " -Ilib/ benchmark.rb " + str(k) + " > /dev/null")
-        if mode == 3: os.system(truffleruby + " benchmark_polyglot.rb " + str(k) + " > /dev/null")
-
-
+    for mode in modes:
+        if os.path.exists("bench.txt"):
+            os.remove("bench.txt")
+        if mode == PYCALL_RB:
+            os.system("ruby benchmark.rb " + str(k) + " > /dev/null")
+        if mode == TRUFFLERUBY_POLYGLOT:
+            os.system(truffleruby + " -Ilib/ benchmark.rb " + str(k) + " > /dev/null")
+        if mode == TRUFFLERUBY_PYCALL:
+            os.system(truffleruby + " benchmark_polyglot.rb " + str(k) + " > /dev/null")
         result = "failed"
         if os.path.exists("bench.txt"):
             with open("bench.txt") as f:
                 result = f.read().strip()
-
         print(result, end='', flush=True)
         if mode != 3: print(',', end='', flush=True)
     print("")
